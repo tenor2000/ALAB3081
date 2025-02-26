24fc577a0bd712f4eeaa4daab8603e228f7fb5cf
tenor2000 <tenor2000@gmail.com>
2025-02-25 21:14:24 -0500

    part 1 completed

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = n1 + n2 + n3 + n4 == 50;
+console.log(`The four numbers add up to 50: ${isSum50}`);
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(`The four numbers have at least two odd numbers: ${isTwoOdd}`);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+// CHANGED AS PER DIRECTIONS
+const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+console.log(`The four numbers are under 25: ${isUnder25}`);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -26,15 +30,41 @@ const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 // that we will review later.
 const isUnique =
   n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(`The four numbers are unique: ${isUnique}`);
+
+// MY WORK HERE
+
+// Check five: check if all numbers divisble by 5
+// Using modulus, I can check if divisible by 5, if remainder is 0
+const isDivisibleBy5 =
+  n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
+console.log(`The four numbers are divisible by 5: ${isDivisibleBy5}`);
+
+// Check six: check if first number is larger than the last number
+// Using > operator to check if first number is larger than the last number
+const isFirstLargerLast = n1 > n4;
+console.log(
+  `The first number is larger than the last number: ${isFirstLargerLast}`
+);
+
+// Check seven: Accomplish Arithmetic chain
+// Order of operations for this chain with the modulus operator returning the remainder:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+const firstResult = n1 - n2;
+const secondResult = firstResult * n3;
+const finalResult = secondResult % n4;
+console.log(`The final result of the arithmetic chain is: ${finalResult}`);
 
 // Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+//const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+//console.log(isValid);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
